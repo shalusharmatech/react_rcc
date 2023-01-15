5306c83d6c8096a1176bde9a0a8b163648cb90bf
Tushar Chopra <tusharchopratech@gmail.com>
2023-01-14 21:19:52 -0500

    navigation page number check added

--- src/components/Products/Products.fnmock.test.js
+++ src/components/Products/Products.fnmock.test.js
@@ -1,8 +1,7 @@
-import { fireEvent, screen, render, waitFor } from "@testing-library/react";
+import { fireEvent, screen, render, waitFor, act } from "@testing-library/react";
 import Products from "./Products";
-import axiosMock from "axios";
-import { act } from "@testing-library/react";
 import * as productUtils from "./ProductUtils";
+
 
 jest.mock("./ProductUtils", () => {
   return {
@@ -11,7 +10,7 @@ jest.mock("./ProductUtils", () => {
 });
 
 describe("Testing Products List.", () => {
-
+  // let renderMUI;
   const mockProducts = {
     products: [
       {
@@ -29,11 +28,12 @@ describe("Testing Products List.", () => {
         thumbnail: "...",
       },
     ],
-    total: 2
+    totalPages: 1
   };
 
   beforeEach(() => {
     productUtils.getProductsData.mockImplementation(() => mockProducts);
+    // renderMUI = createRender();
     console.log("Before Each TEST I run.");
   });
 
@@ -58,11 +58,17 @@ describe("Testing Products List.", () => {
     act(() => {
       render(<Products />);
     });
-
+    
     await waitFor(() => {
       const paginationElement = screen.getByTestId("pagination-id");
       expect(paginationElement).toBeInTheDocument();
+      let pages = paginationElement.getElementsByTagName("li");
+
+      // Note: +2 is for two arrows.
+      // Total <li> elements = totalPages + left arrow (<) and right arrow (>).
+      expect(pages.length).toBe(mockProducts.totalPages + 2);
     });
+
   });
 
 
